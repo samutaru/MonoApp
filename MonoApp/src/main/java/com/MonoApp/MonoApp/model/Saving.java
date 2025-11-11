b06e6669e3e1adba60208e6c1c47fb294cc0577a
samuel tamayo ruiz <samutaru202@gmail.com>
2025-11-11 17:44:59 +0200

    Backend with db conected and teorically working logic still td

--- MonoApp/src/main/java/com/MonoApp/MonoApp/model/Saving.java
+++ MonoApp/src/main/java/com/MonoApp/MonoApp/model/Saving.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import java.util.UUID;
 
 @Entity
+@Table(name = "savings")
 public class Saving {
 
     @Id
--- MonoApp/src/main/java/com/MonoApp/MonoApp/model/User.java
+++ MonoApp/src/main/java/com/MonoApp/MonoApp/model/User.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
